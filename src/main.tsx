385eb960bc66c23baa6cff7633ad09218f90c12f
Chathumina Bandara <vnnethsara2002@gmail.com>
2026-01-01 16:12:52 +0530

    m

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,11 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { ThemeProvider } from './contexts/ThemeContext';
-import { ErrorBoundary } from './components/ErrorBoundary';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error('Root element not found');
-}
-
-createRoot(rootElement).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </ErrorBoundary>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
